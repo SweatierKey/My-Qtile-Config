f89ab1f9d4422d9405c7c8ac5ede12b7507b637d
SweatierKey <matteoaquino17@gmail.com>
2021-09-10 23:33:42 +0200

    1

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-sudo pacman -S xorg-server qtile alacritty discord mpv noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra neofetch python-pipx python-psutil lightdm lightdm-gtk-greeter zathura zathura-cb zathura-pdf-mupdf wine-staging wine-mono wine-gecko virtualbox virtualbox-host-dkms vagrant thunar lutris dialog steam redshift pavucontrol ntfs-3g android-file-transfer
+sudo pacman -S --needed xorg-server qtile alacritty discord mpv noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra neofetch python-pipx python-psutil lightdm lightdm-gtk-greeter zathura zathura-cb zathura-pdf-mupdf wine-staging wine-mono wine-gecko virtualbox virtualbox-host-dkms vagrant thunar lutris dialog steam redshift pavucontrol ntfs-3g android-file-transfer
+
+cp bashrc/.bashrc /home/$(whoami)/
+cp wallpaper/ /home/$(whoami)/
+cp qtile/config.py /home/$(whoami)/.config/
 
