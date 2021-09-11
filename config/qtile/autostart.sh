e0ba2674bb8c9432b40b2139e47c4b7d8c65298b
SweatierKey <matteoaquino17@gmail.com>
2021-09-11 15:53:51 +0200

    1

--- config/qtile/autostart.sh
+++ config/qtile/autostart.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-feh --bg-fill -z /home/matteo/.config/wallpaper/
+feh --bg-fill -z /home/matteo/.config/wallpaper/ &
+killall redshift &
+redshift -t 6500:4500 -l 40.8563100:14.2464100 &
+
--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo pacman -S --needed xorg-server qtile rofi papirus-icon-theme alacritty discord mpv noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra neofetch python-pipx python-psutil lightdm lightdm-gtk-greeter zathura zathura-cb zathura-pdf-mupdf wine-staging wine-mono wine-gecko virtualbox virtualbox-host-dkms vagrant thunar lutris dialog steam redshift pavucontrol ntfs-3g android-file-transfer
+sudo pacman -S --needed xorg-server qtile rofi papirus-icon-theme feh alacritty discord mpv noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra neofetch python-pipx python-psutil lightdm lightdm-gtk-greeter zathura zathura-cb zathura-pdf-mupdf wine-staging wine-mono wine-gecko virtualbox virtualbox-host-dkms vagrant thunar lutris dialog steam redshift pavucontrol ntfs-3g android-file-transfer
 
 cp -r bashrc/.bashrc /home/$(whoami)/
+mkdir /home/$(whoami)/.config
 cp -r config/* /home/$(whoami)/.config/
